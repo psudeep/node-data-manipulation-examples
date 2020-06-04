c9eaafde53628e8ff3c856f12e0a9ba728f5ffcc
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-06-04 16:56:13 +0530

    examples has been set

---
+++ examples/arrayMerge.js
@@ -0,0 +1,39 @@
+const array1 = [{
+  key: 'val1',
+  key2: 'val2',
+  name: 'prashant',
+  mobile: 'xxxxxxxxxxx',
+  gender: 'male'
+}{
+  key: 'twst',
+  key2: 'twst2',
+  name: 'psudep',
+  mobile: '1111111',
+  gender: 'female'
+}{
+  key: 'qeuu',
+  key2: 'hello',
+  name: 'sipu',
+  mobile: '99999',
+  gender: 'male'
+}];
+
+const array2 = [{
+  key: 'tesing',
+  key2: 'tesing2',
+  name: 'sudeep',
+  mobile: '9999999999',
+  gender: 'female'
+}{
+  key: 'now',
+  key2: 'now2',
+  name: 'cpoo',
+  mobile: '1111111',
+  gender: 'male'
+}];
+
+//this function to merge above 2 arrays and create a new JSON arrays with total 5 Objects 3 from first and 2 from second one
+
+function mergeArrays(arg1, arg2) {
+
+};
